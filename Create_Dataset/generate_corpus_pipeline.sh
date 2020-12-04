cb2b6028579417bce55f252846fd17f7934b68f6
aemami1 <33097267+aemami1@users.noreply.github.com>
2020-12-04 15:18:01 -0500

    Update generate_corpus_pipeline.sh

--- Create_Dataset/generate_corpus_pipeline.sh
+++ Create_Dataset/generate_corpus_pipeline.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 ACTION=$1
 
-if [ "$ACTION" == "split" ] ; then
+if [ "$ACTION" == "split" ] || [ "$ACTION" == "oneshot" ] ; then
      python knowref_scraper/sources/split_sentences.py --mode pronoun knowref_scraper/sources/reddit knowref_scraper/sources/data/wp_pronoun_owndump.txt --n-jobs 1 --chunk-size 120
 fi
 
-if [ "$ACTION" == "postag" ] ; then
+if [ "$ACTION" == "postag" ] || [ "$ACTION" == "oneshot" ] ; then
     cd stanford-postagger-full-2020-11-17
     java -cp "*" edu.stanford.nlp.tagger.maxent.MaxentTagger -model models/english-left3words-distsim.tagger -textFile ../knowref_scraper/sources/data/wp_pronoun_owndump.txt -outputFormat slashTags -outputFile ../knowref_scraper/sources/data/wp_pronoun_owndump_pos.txt -nthreads 24
     cd ..
 fi
 
-if [ "$ACTION" == "postag_filter" ] ; then
+if [ "$ACTION" == "postag_filter" ] || [ "$ACTION" == "oneshot" ] ; then
     python knowref_scraper/sources/filter_postagged.py knowref_scraper/sources/data/wp_pronoun_owndump_pos.txt knowref_scraper/sources/data/wp_pronoun_owndump_pos_filtered_0.txt --mode pronoun --n-jobs -1
     awk '!(count[$0]++)' knowref_scraper/sources/data/wp_pronoun_owndump_pos_filtered_0.txt > knowref_scraper/sources/data/wp_pronoun_owndump_pos_filtered.txt
     rm knowref_scraper/sources/data/wp_pronoun_owndump_pos_filtered_0.txt
 fi
 
-if [ "$ACTION" == "corenlp" ] ; then
+if [ "$ACTION" == "corenlp" ] || [ "$ACTION" == "oneshot" ] ; then
     cd stanford-corenlp-full-2018-10-05
     mkdir -p sentences
     perl -ne '/^(.*?)\|/; $_=$1; s/[_\[\]]/ /g; print "$_\n"' ../knowref_scraper/sources/data/wp_pronoun_owndump_pos_filtered.txt > wp_pronoun_owndump_inputs.txt
@@ -27,7 +27,7 @@ if [ "$ACTION" == "corenlp" ] ; then
     cd ..
 fi
 
-if [ "$ACTION" == "final" ] ; then
+if [ "$ACTION" == "final" ] || [ "$ACTION" == "oneshot" ] ; then
         python  knowref_scraper/sources/filter_parsed_pronoun_knowref.py  "stanford-corenlp-full-2018-10-05/wp_pronoun_owndump_*.xml" knowref_scraper/sources/data/wp_pronoun_owndump_final_reddit.json --n-jobs 1
 fi
 
